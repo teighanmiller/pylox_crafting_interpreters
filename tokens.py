0e750ab4ef0815cf24489250430cfe8cf0a71518
Teighan Miller <teighanmiller@mac.lan>
2025-05-23 21:57:45 -0400

    added __init__ and to_string functions to Token class

--- tokens.py
+++ tokens.py
@@ -1,8 +1,17 @@
 import Lox
 from typing import Final
-from TokenType import TokenType
+from token_type import TokenType
 
 class Token:
     type: Final[TokenType]
+
+    def __init__(self, type: TokenType, lexeme: str, literal: object, line: int):
+        self.type = type
+        self.lexeme = lexeme
+        self.literal = literal
+        self.line = line
+
+    def to_string(self):
+        return self.type + " " + self.lexeme + " " + self.literal
     
     
