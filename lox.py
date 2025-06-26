82a6ff019b9a0190f1166bf29f6c6b0a28c22bc5
Teighan Miller <teighanmiller@Teighans-Laptop.local>
2025-06-26 07:49:22 -0400

    adding comments and fixing syntax errors

--- lox.py
+++ lox.py
@@ -6,17 +6,18 @@ class Lox:
 
     def run_file(self, path):
         try:
+            # read the file passed in the argument
             with open(path, "r") as f:
                 data = f.readline()
                 self._run(data)
+        # Error handling if file cannot be read.
         except IOError as e:
             print(f"Error reading file: {e}")
             sys.exit(65)
 
     def run_prompt(self):
         while(True):
-            print("< ")
-            cmd = input()
+            cmd = input("> ")
             if cmd is None:
                 break
             self._run(cmd)
@@ -36,13 +37,17 @@ class Lox:
         print(f"[line {line}] Error{where}: {message}")
         self.had_error = True
 
-if __name__ == "__main___":
+if __name__ == "__main__":
     lox = Lox()
 
-    if(sys.argv[1:] > 1):
-        print("Usage: pylox[script]")
+    # Check if the length of the command call.
+    # If more than one argument is passed exit.
+    if len(sys.argv[1:]) > 1:
+        print("Usage: pylox [script]")
         sys.exit(64)
-    elif sys.argv[1:] == 1:
-        lox.run_file(sys.argv[1])
+    # If one argument is passed run the file
+    elif len(sys.argv[1:]) == 1:
+        lox.run_file(sys.argv[0])
+    # If no arguments are passed run the prompt
     else:
         lox.run_prompt()
